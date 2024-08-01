72ea1c05de39bd1b4dbf3c5b9b9d444243b98b85
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-08-01 12:24:30 +0530

    import css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
-
+import './stylesheets/alignments.css'
+import './stylesheets/custom-components.css'
+import './stylesheets/form-elements.css'
+import './stylesheets/text-elements.css'
+import './stylesheets/theme.css'
 
 function App() {
   return (
